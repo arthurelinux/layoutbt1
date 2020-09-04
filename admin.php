5e6204a70c440873146e5734b6a3322885222cef
Arthur <educadoresnolinux@gmail.com>
2020-09-04 10:03:00 -0300

    novo admin

---
+++ admin.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <title>Layout</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
+
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+       <div class="menu">
+         <nav class="navbar navbar-expand-lg navbar-dark fixed-top menu">
+        
+            <button class="navbar-toggler m-auto" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse m-auto " id="navbarText">
+              <ul class="navbar-nav m-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+                </li>
+              </ul>
+             
+            </div>
+          </nav>
+       </div>
+        <div class="container mt-5 pt-5">
+            <h3>Bem vindo</h3>
+
+            <div class="row">
+                <div class="col col-md-3">
+                    <!-- Card -->
+                <div class="card text-center">
+                    <div class="card-header">
+                        Featured
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div>
+                </div>
+                <div class="col col-md-3">
+                    <!-- Card -->
+                <div class="card text-center">
+                    <div class="card-header">
+                        Featured
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div>
+                </div>
+                <div class="col col-md-3">
+                <!-- Card -->
+                <div class="card text-center">
+                    <div class="card-header">
+                        Featured
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div>
+                </div>
+                <div class="col col-md-3">
+                    <!-- Card -->
+                <div class="card text-center">
+                    <div class="card-header">
+                        Featured
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div>
+                </div>
+
+            </div>
+
+        </div>
+
+       <div class="footer text-center p-4">
+            Softar 2020
+       </div>
+    
+        <script src="bootstrap/js/jquery-3.3.1.slim.min.js"></script>
+        <script src="bootstrap/js/popper.min.js" ></script>
+        <script src="bootstrap/js/bootstrap.min.js" ></script>
+    </body>
+</html>
